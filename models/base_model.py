1af4ca9f76f188e6219304a2f2a5f1c7db130818
Dawitt-dev <davoza27@yahoo.com>
2024-02-09 08:39:31 -0800

    Task_4.Created BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -13,10 +13,20 @@ class BaseModel():
         created_at:
         updated_at:
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """The base class constructer"""
+        if kwargs != {}:
+            date_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.strptime(value, date_format)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
     
     def save(self):
         """A public instance method that updates "updated_at" attribute"""
@@ -24,20 +34,18 @@ class BaseModel():
     
     def to_dict(self):
         """A public instance method that returns a dictionary"""
-        class_name = self.__class__.__name__
-        dictionary_rep = {
-                "__class__": class_name,
-                "id": self.id,
-                "created_at": self.created_at.isoformat(),
-                "updated_at": self.updated_at.isoformat()
-        }
+        dictionary_rep = self.__dict__.copy()
+        dictionary_rep['__class__'] = self.__class__.__name__
+        dictionary_rep['created_at'] = self.created_at.isoformat()
+        dictionary_rep['updated_at'] = self.updated_at.isoformat()
         return dictionary_rep
     
     def __str__(self):
-        class_name = self.__class__.__name__
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
 #obj = BaseModel()
+#my_obj_dict = obj.to_dict()
+#print(my_obj_dict)
 #print(obj.id)
 #print(obj.created_at)
 #print(obj.updated_at)
