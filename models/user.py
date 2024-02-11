c35d88b3c2ab0c78bce7030c4eb02d6db94c8bd6
Dawitt-dev <davoza27@yahoo.com>
2024-02-10 20:33:39 -0800

    improved with docstrings

--- models/user.py
+++ models/user.py
@@ -5,7 +5,13 @@ import json
 
 
 class User(BaseModel):
-    """subclass of the BaseModel class"""
+    """subclass of the BaseModel class
+    Attributes:
+         email (str) -> User's email
+         password (str) -> User's password
+         first_name (str) -> User's first Name
+         last_name (str) -> User's last Name
+    """
     
     email = " "
     password = " "
