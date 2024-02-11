061a259ff57acd5e8818d9a76ea22647d2a10163
Dawitt-dev <davoza27@yahoo.com>
2024-02-11 06:02:17 -0800

    file storage updated for task 10

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,12 +3,26 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class FileStorage():
     """ the FileStorage class of the AirBnB Clone Project"""
     __file_path = "file.json"
     __objects = {}
-    class_dict = {"BaseModel": BaseModel, "User": User}
+    class_dict = {
+        "BaseModel": BaseModel, 
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review":  Review,
+    }
+
     def __init__(self):
         """initiates file_storage class"""
         pass
